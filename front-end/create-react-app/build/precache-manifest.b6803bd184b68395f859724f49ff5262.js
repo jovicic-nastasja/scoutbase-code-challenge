ec4e0223f383bf63c992bf0f1b3fbbb31ea169c3
Nastasja Jovicic <jovicic.nastasja@gmail.com>
2019-10-05 17:54:31 +0300

    added build

---
+++ front-end/create-react-app/build/precache-manifest.b6803bd184b68395f859724f49ff5262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e07a658d0ea7b64f7a29d80ca720c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4ec16739f5fb74eb053",
+    "url": "/static/css/main.818b8979.chunk.css"
+  },
+  {
+    "revision": "dbc37c5d9b787df0f974",
+    "url": "/static/js/2.8eae2e4f.chunk.js"
+  },
+  {
+    "revision": "f4ec16739f5fb74eb053",
+    "url": "/static/js/main.65de8cdb.chunk.js"
+  },
+  {
+    "revision": "90ec748c06606314ad4c",
+    "url": "/static/js/runtime-main.8df5b11f.js"
+  }
+]);
